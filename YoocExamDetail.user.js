6ea9bbfc0d9d734ba3503159d9d0ff5ab331e6d6
Avenshy <30952834+Avenshy@users.noreply.github.com>
2020-07-21 16:37:03 +0800

    upload v0.2

--- YoocExamDetail.user.js
+++ YoocExamDetail.user.js
@@ -1,17 +1,28 @@
 // ==UserScript==
 // @name         易班优课考试查卷
 // @namespace    YoocExamDetail
-// @version      0.1
+// @version      0.2
 // @description  易班优课考试查卷，让禁止查卷的考试也能查卷。（不是在考试前偷看试卷！）
 // @author       Avenshy
 // @homepageURL  https://github.com/Avenshy/YoocExamDetail
 // @supportURL   https://github.com/Avenshy/YoocExamDetail/issues
-// @match        https://www.yooc.me/group/*/exams
+// @match        https://www.yooc.me/group/*
 // @license      GPL-3.0
 // @grant        none
 // ==/UserScript==
 (function() {
     'use strict';
+    let tabs = document.querySelector('.aside-nav').querySelectorAll('a');
+    for (let tab of tabs) {
+        if (tab.querySelector('p').textContent == '在线考试') {
+            tab.addEventListener('click', () => {
+                if (document.querySelector('.ctx-header').querySelector('h1').textContent != '在线考试') {
+                    location.reload(true);
+                }
+            },
+            false);
+        }
+    }
     let groupId = document.querySelector('#group-data').getAttribute('data-group-id');
     let examList = document.querySelectorAll('.done.clearfix');
     for (let exam of examList) {
@@ -20,9 +31,10 @@
         let text = scoreElement.textContent;
         if (text == "禁止查卷") {
             scoreElement.textContent = "点击查卷";
-            scoreElement.addEventListener('click',()=>{
+            scoreElement.addEventListener('click', () => {
                 window.open(`https://www.yooc.me/group/${groupId}/exam/${examId}/detail`,'_blank');
-            },false);
+            },
+            false);
         }
     }
 })();
